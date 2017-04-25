62a9c609e50dbc12e85ebe8c214beb088e27da38
Acha Bill <achabill12@gmail.com>
2017-04-25 16:04:21 +0100

    updates

--- PFE_WPF/LogIn.xaml.cs
+++ PFE_WPF/LogIn.xaml.cs
@@ -6,6 +6,8 @@ using Firebase.Auth;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Net.Http;
+using Firebase.Database;
+
 namespace PFE_WPF
 {
     public partial class LogIn : Window
@@ -14,6 +16,7 @@ namespace PFE_WPF
         public const string GoogleClientId = "<GOOGLE CLIENT ID>"; // https://console.developers.google.com/apis/credentials
         public const string FirebaseAppKey = "<FIREBASE APP KEY>"; // https://console.firebase.google.com/
         public const string FirebaseAppUrl = "https://applicationcliente.firebaseio.com/";
+        public const string FirebaseApiKey = "AIzaSyCvoKY814M1ipT2A2OPp16VJCxRKqAXkpQ";
 
         public LogIn()
         {
@@ -22,14 +25,55 @@ namespace PFE_WPF
 
         private async void Connect_Button(object sender, RoutedEventArgs e)
         {
-            await initializeConnexion();
+            bool loginSuccess = true;
+            try
+            {
+                //To test, first run SignupAsync, then run LoginAsync
+                //For some reason, both Login and Signup return error.
+                //You might need to check your console.
+
+
+                //await InitializeConnexionAsync(LoginAsync);
+                await InitializeConnexionAsync(SignupAsync);
+            }
+            catch (Exception ex)
+            {
+                loginSuccess = false;
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                if (loginSuccess)
+                    MessageBox.Show("Success");
+            }
         }
-        public async Task initializeConnexion()
-          {
-        String em = email.ToString();
-        String pass = password.ToString();
-        var authProvider = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyCvoKY814M1ipT2A2OPp16VJCxRKqAXkpQ"));
-        var auth = await authProvider.SignInWithEmailAndPasswordAsync(em, pass);
+        public async Task InitializeConnexionAsync(Func<string,string,Task<string>> action)
+        {
+            string em = email.Text;
+            string pass = password.Password;
+            var firebase = new FirebaseClient(FirebaseAppUrl, new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = () => action(em, pass)
+            });
+
+            var result = await firebase.Child("/").OnceAsync<object>();
+            MessageBox.Show(result.Count.ToString());
+
+        }
+        private static async Task<string> LoginAsync(string email, string password)
+        {
+            //This is normallly done on the server.
+            //Exposing FirebaseApiKey on the client is not good.
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(FirebaseApiKey));
+            var auth = await authProvider.SignInWithEmailAndPasswordAsync(email, password);
+            return auth.FirebaseToken;
+        }
+
+        private static async Task<string> SignupAsync(string email, string password)
+        {
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(FirebaseApiKey));
+            var auth = await authProvider.CreateUserWithEmailAndPasswordAsync(email, password);
+            return auth.FirebaseToken;
         }
     }
 }
