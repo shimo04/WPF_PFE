243fa1b6b400e7c351846f3bb3e80670d1291808
shayma <mejrishayma0@gmail.com>
2017-04-25 04:38:29 -0700

    initializeConnexion

--- PFE_WPF/LogIn.xaml.cs
+++ PFE_WPF/LogIn.xaml.cs
@@ -1,25 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Firebase.Auth;
+using Newtonsoft.Json;
+using System.Diagnostics;
 using System.Net.Http;
-using System.Net;
-using System.IO;
-
 namespace PFE_WPF
 {
     public partial class LogIn : Window
     {
+
         public const string GoogleClientId = "<GOOGLE CLIENT ID>"; // https://console.developers.google.com/apis/credentials
         public const string FirebaseAppKey = "<FIREBASE APP KEY>"; // https://console.firebase.google.com/
         public const string FirebaseAppUrl = "https://applicationcliente.firebaseio.com/";
@@ -29,20 +20,16 @@ namespace PFE_WPF
             InitializeComponent();
         }
 
-        private void Connect_Button (object sender, RoutedEventArgs e)
+        private async void Connect_Button(object sender, RoutedEventArgs e)
         {
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(new
-            {
-                cc = "vv",
-            });
-          // var request = WebRequest.CreateHttp("https://applicationcliente.firebaseio.com/.json");
-          //  request.Method = "POST";
-          //  request.ContentType = "application/json";
-           // var buffer = Encoding.UTF8.GetBytes(json);
-           // request.ContentLength = buffer.Length;
-          // request.GetRequestStream().Write(buffer, 0, buffer.Length);
-           // var response = request.GetResponse();
-            //json = (new StreamReader(response.GetResponseStream())).ReadToEnd();
+            await initializeConnexion();
+        }
+        public async Task initializeConnexion()
+          {
+        String em = email.ToString();
+        String pass = password.ToString();
+        var authProvider = new FirebaseAuthProvider(new FirebaseConfig("AIzaSyCvoKY814M1ipT2A2OPp16VJCxRKqAXkpQ"));
+        var auth = await authProvider.SignInWithEmailAndPasswordAsync(em, pass);
         }
     }
 }
